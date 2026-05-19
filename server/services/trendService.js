fa1bf809506ac43f099663a8ed5571fdb1ce85cc
Khalil Elmariami <elmariamikhalil@gmail.com>
2026-05-19 14:55:49 +0100

    fix: dotenv path in all services + complete UI redesign for readability

--- server/services/trendService.js
+++ server/services/trendService.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
 const axios = require('axios');
 const RSSParser = require('rss-parser');
 
